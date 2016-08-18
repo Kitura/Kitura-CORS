5644606fc0fbddf957c38ec07fa3337dca4fb3cf
Shmuel Kallner <kallner@il.ibm.com>
2016-08-18 14:20:34 +0300

    Fixed Kitura dependency version

--- Package.swift
+++ Package.swift
@@ -19,6 +19,6 @@ import PackageDescription
 let package = Package(
     name: "KituraCORS",
         dependencies: [
-            .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 0, minor: 25),
+            .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 0, minor: 26),
          ]
 )
